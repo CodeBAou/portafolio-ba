8e3b72e131ae32f226dd011b35be4d91e662f7f3
CodeBAou <borisafou@gmail.com>
2024-01-23 21:03:59 +0100

    [next-auth][error][NO_SECRET]

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,9 +7,10 @@ const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.SECRET || ''
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     })
-  ]
+  ],
+  secret: process.env.SECRET
 })
 
 export { handler as GET, handler as POST }
